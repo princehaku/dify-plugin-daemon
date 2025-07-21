3b0a8679f4461185bbc0f5b5710159500b7f8a8c
Maries <xh001x@hotmail.com>
2025-07-21 15:37:13 +0800

    feat/tool oauth cli template (#407)
    * feat(cli): update OAuth handling and requirements for dify_plugin
    
    * feat(oauth): update OAuth support and adjust dify_plugin version constraints

--- cmd/commandline/plugin/templates/python/tool_provider.py
+++ cmd/commandline/plugin/templates/python/tool_provider.py
@@ -5,6 +5,7 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
 
 class {{ .PluginName | SnakeToCamel }}Provider(ToolProvider):
+    
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
             """
@@ -12,3 +13,33 @@ class {{ .PluginName | SnakeToCamel }}Provider(ToolProvider):
             """
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
+
+    #########################################################################################
+    # If OAuth is supported, uncomment the following functions.
+    # Warning: please make sure that the sdk version is 0.4.2 or higher.
+    #########################################################################################
+    # def _oauth_get_authorization_url(self, redirect_uri: str, system_credentials: Mapping[str, Any]) -> str:
+    #     """
+    #     Generate the authorization URL for {{ .PluginName }} OAuth.
+    #     """
+    #     try:
+    #         """
+    #         IMPLEMENT YOUR AUTHORIZATION URL GENERATION HERE
+    #         """
+    #     except Exception as e:
+    #         raise ToolProviderOAuthError(str(e))
+    #     return ""
+        
+    # def _oauth_get_credentials(
+    #     self, redirect_uri: str, system_credentials: Mapping[str, Any], request: Request
+    # ) -> Mapping[str, Any]:
+    #     """
+    #     Exchange code for access_token.
+    #     """
+    #     try:
+    #         """
+    #         IMPLEMENT YOUR CREDENTIALS EXCHANGE HERE
+    #         """
+    #     except Exception as e:
+    #         raise ToolProviderOAuthError(str(e))
+    #     return dict()
